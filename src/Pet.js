26432e4a749833aed677e476670fb4fa21dc5e8f
connerv42 <connerverret42@gmail.com>
2018-12-19 13:28:05 -0800

    add HobbyList

--- src/Pet.js
+++ src/Pet.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import HobbyList from './HobbyList';
 import "./Pet.css";
 
 class Pet extends Component { // Every component will extend this
@@ -9,6 +10,7 @@ class Pet extends Component { // Every component will extend this
                 <img src="https://github.com/connerv42/Photos/raw/master/20161008_153544.jpg"
                     alt="moxie my cat" />
                 <h5 style={{ fontSize: '2em', margin: '2px' }}>Hobbies:</h5>
+                <HobbyList />
             </div>
         )
     }
