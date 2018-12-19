3e6bc850b7f2efa0cc26fdce1eb786dcad2626a2
connerv42 <connerverret42@gmail.com>
2018-12-19 11:57:47 -0800

    Made img tag's src load correctly

--- src/Pet.js
+++ src/Pet.js
@@ -6,7 +6,7 @@ class Pet extends Component { // Every component will extend this
         return (
             <div className="card">
                 <h2 className="name">Jax</h2>
-                <img src="./20161008_153544.jpg"
+                <img src="https://github.com/connerv42/Photos/raw/master/20161008_153544.jpg"
                     alt="moxie my cat" />
                 <h5 style={{ fontSize: '2em', margin: '2px' }}>Hobbies:</h5>
             </div>
