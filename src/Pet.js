230c436794b75b3a03f4d02f5af5ab3dd843ecb2
connerv42 <connerverret42@gmail.com>
2018-12-19 11:40:41 -0800

    First Commit

---
+++ src/Pet.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import "./Pet.css";
+
+class Pet extends Component { // Every component will extend this
+    render() {
+        return (
+            <div className="card">
+                <h2 className="name">Jax</h2>
+                <img src="./20161008_153544.jpg"
+                    alt="moxie my cat" />
+                <h5 style={{ fontSize: '2em', margin: '2px' }}>Hobbies:</h5>
+            </div>
+        )
+    }
+}
+
+export default Pet;
